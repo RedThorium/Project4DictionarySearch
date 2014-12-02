d8b02ca9f773b90265f86f6700c394dd960438c7
RedThorium <alexhowinwong@gmail.com>
2014-12-02 03:56:14 -0500

    12/2/14 2 am

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,4 +1,39 @@
+import java.util.*;
 
-public class BinarySearchTree {
-
+public class BinarySearchTree<T extends Comparable<T>> implements Iterator<T>{
+	protected BSTNode<T> root;
+	
+	public void add(T data){
+		root = redAdd(root, data);
+	}
+	
+	/**
+	 * The recursive portion of the add method 
+	 * @return
+	 */
+	private BSTNode<T> redAdd(BSTNode<T> node, T newData){
+		if (node == null){
+			BSTNode<T> newNode = new BSTNode<T>(newData);
+			return newNode;
+		}
+		if (newData.compareTo(node.getData()) < 0){
+			node.setLeft(redAdd(node.getLeft(), newData));
+		}
+		else{
+			node.setRight(redAdd(node.getRight(), newData))
+		}
+		return node;
+	}
+	
+	public T remove(){
+		
+	}
+	
+	public void contains(T item){
+		
+	}
+	
+	public ArrayList<T> iterator(){
+		
+	}
 }
